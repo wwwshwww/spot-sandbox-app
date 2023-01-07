587e17f9df4f8b263ad01058d756d07645a6ec32
wwwshwww <www.shinderu.www@gmail.com>
2023-01-07 14:06:16 +0900

    change marker shape

--- ui/react-app/src/components/atoms/SpotMarker.tsx
+++ ui/react-app/src/components/atoms/SpotMarker.tsx
@@ -14,19 +14,19 @@ export const SpotMarker: React.FC<SpotMarkerProps> = (props) => {
   return (
     <Marker
       icon={{
-        path: 'M0,0 l-1,-8 S-10,-18 0,-20. M0,0 l1,-8 S10,-18 0,-20 ',
+        path: ' M 0 0 L -10 -30 A 10 12 1 0 1 10 -30 Z ',
         fillColor: 'hsl(' + props.baseColor + ', 100%, 75%)',
-        fillOpacity: 0.8,
+        fillOpacity: 0.9,
         strokeWeight: 1,
-        strokeColor: 'hsl(' + props.baseColor + ', 80%, 50%)',
-        scale: 2,
-        labelOrigin: new google.maps.Point(0, -15),
+        strokeColor: 'hsl(' + props.baseColor + ', 40%, 45%)',
+        scale: 1,
+        labelOrigin: new google.maps.Point(0, -31),
       }}
       position={{ lat: props.latlng.lat, lng: props.latlng.lng }}
       label={{
         color: '#333333',
         text: props.labelText,
-        fontSize: '10px',
+        fontSize: '11px',
         fontWeight: '700',
       }}
     />
